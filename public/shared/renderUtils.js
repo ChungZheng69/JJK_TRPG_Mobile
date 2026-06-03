cb378454a9b32ccf603b7d5c7de302374adaf15a
ChungZheng69 <100736279+ChungZheng69@users.noreply.github.com>
2026-06-03 23:11:16 +0800

    Delete public directory

--- public/shared/renderUtils.js
+++
@@ -1,9 +0,0 @@
-export {
-  addMessage,
-  renderAiMessageSections
-} from "../js/render/renderChat.js";
-
-export {
-  QUICK_ACTIONS,
-  insertQuickAction
-} from "../js/render/renderQuickActions.js";
